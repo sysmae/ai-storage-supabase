f2d4bd1dfe41f5e545ee3316cee616b04fe6cc6a
sysmae <j970330@gmail.com>
2024-12-30 05:23:30 +0900

    이름 및 로그인 시 /prompts로 이동하게 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import DeployButton from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
@@ -42,12 +41,10 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"}>Next.js Supabase Starter</Link>
-                    <div className="flex items-center gap-2">
-                      <DeployButton />
-                    </div>
+                    <Link href={"/"}>온라인 프롬프트 모음</Link>
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                  <ThemeSwitcher />
                 </div>
               </nav>
               <div className="flex flex-col gap-20 max-w-5xl p-5">
@@ -55,7 +52,7 @@ export default function RootLayout({
               </div>
 
               <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
+                {/* <p>
                   Powered by{" "}
                   <a
                     href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
@@ -65,7 +62,7 @@ export default function RootLayout({
                   >
                     Supabase
                   </a>
-                </p>
+                </p> */}
                 <ThemeSwitcher />
               </footer>
             </div>
